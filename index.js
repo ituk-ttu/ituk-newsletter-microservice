1dae87b066acbc9b8afa71f65ba25adf963b3b22
Johannes Kümmel <jkymmel@gmail.com>
2017-08-15 20:51:05 +0300

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var request = require('request');
+var fs = require("fs");
+var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var config = JSON.parse(fs.readFileSync("config.json"));
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
+app.use(cors());
+
+app.post('/signup', function (req, res) {
+    request({
+        method: "POST",
+        url: config.newsletter.signupUrl,
+        json: true,
+        body: {
+            list_id: config.newsletter.listId,
+            subscriber: req.body.email,
+            pre_confirmed: false,
+            pre_approved: true
+        }
+    }, function (response) {
+        res.sendStatus(200);
+    });
+});
+
+http.listen(config.port, function(){
+    console.log('listening on *:' + config.port);
+});
